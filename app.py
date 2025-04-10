54f047b0c766b72b596a63ec9944f0f417209be6
Pritesh Rajput <rajputpritesh01@gmail.com>
2025-04-10 11:02:45 +0530

    Added favicon support

--- app.py
+++ app.py
@@ -21,6 +21,11 @@ transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                              'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     result = None
